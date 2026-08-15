e10297f34b40db87828598072d005d9ad1880a71
Niyas <viberstech@gmail.com>
2026-08-14 23:12:55 -0700

    Update Zowexo website

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/zowexo-web",
+  assetPrefix: "/zowexo-web/",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
