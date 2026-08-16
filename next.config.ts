ce6d3b4e757d757744ef139afc25502ecdebda19
Niyas <viberstech@gmail.com>
2026-08-15 22:32:19 -0700

    Fix Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/zowexo-web",
-  assetPrefix: "/zowexo-web/",
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
